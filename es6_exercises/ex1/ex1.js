0c6d489a66b489a3c50702d46f7e3706f798d8d9
Jaclyn Adams <jaadams05@gmail.com>
2016-03-07 10:22:48 -0800

    refactor code to use new features for block scoping

--- es6_exercises/ex1/ex1.js
+++ es6_exercises/ex1/ex1.js
@@ -1,14 +1,18 @@
+'use strict'
+
 var x = 2, fns = [];
 
-(function(){
-	var x = 5;
+{
+    let x = 5;
 
-	for (var i=0; i<x; i++) {
-		// ..
-	}
-})();
+    for (let i = 0; i < x; i++) {
+        fns.push(function() {
+            return i });
+    }
+};
+
 
 console.log(
-	(x * 2) === fns[x*2]()
+    (x * 2) === fns[x * 2]()
 );
 // true
