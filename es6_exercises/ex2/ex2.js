d2cb6d9bb71ef1bff075a770e9996a931693d9a8
Jaclyn Adams <jaadams05@gmail.com>
2016-03-07 11:07:02 -0800

    implement code to use rest operator

--- es6_exercises/ex2/ex2.js
+++ es6_exercises/ex2/ex2.js
@@ -1,10 +1,15 @@
-function foo() { }
+"use strict"
+
+function foo(x,y,z,...args) { 
+
+	return [x, ...args];
+}
 
 function bar() {
 	var a1 = [ 2, 4 ];
 	var a2 = [ 6, 8, 10, 12 ];
 
-	return foo();
+	return foo(...[...a1, ...a2]);  //2, 4, 6, 8, 10, 12
 }
 
 console.log(
