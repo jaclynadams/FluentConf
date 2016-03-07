b890a8e11bcceada20684fef2bab8a5f31c6d0d4
Jaclyn Adams <jaadams05@gmail.com>
2016-03-07 14:31:52 -0800

    working with template literals

--- es6_exercises/ex4/ex4.js
+++ es6_exercises/ex4/ex4.js
@@ -1,10 +1,19 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	var s = "";
+	for (let i=0; i<strings.length; i++) {
+		if (i > 0) {
+			s += values[i-1].toUpperCase();
+		}
+		s += strings[i];
+	}
+	return s;
+}
 
 var name = "kyle",
 	twitter = "getify",
 	classname = "es6 workshop";
 
 console.log(
-`Hello ____ (@____),
-welcome to the ____!`
+upper`Hello ${name} (@${twitter}),
+welcome to the ${classname}!`
 );
