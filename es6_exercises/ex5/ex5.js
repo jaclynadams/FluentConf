58fc6eaa92815c78ed3eb1647c10138de9e1d078
Jaclyn Adams <jaadams05@gmail.com>
2016-03-07 16:02:33 -0800

    fun with iterators

--- es6_exercises/ex5/ex5.js
+++ es6_exercises/ex5/ex5.js
@@ -1,5 +1,14 @@
 var numbers = {
-	// ..
+	*[Symbol.iterator]({
+			start = 0,
+			max = 100,
+			step = 1,
+		} = {}
+	) {
+		for (let num = start; num <= max; num += step) {
+			yield num;
+		}
+	}
 };
 
 // should print 0..100 by 1s
@@ -8,6 +17,10 @@ for (let num of numbers) {
 }
 
 // should print 6..30 by 4s
-for (let num of /*..*/) {
+for (let num of numbers[Symbol.iterator]({
+	start: 6,
+	max: 30,
+	step: 4,
+})) {
 	console.log(num);
 }
